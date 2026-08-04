1e494beeed05420d5e3982799920158405e7a3a4
abdulrazie <abdulrazie987@gmail.com>
2026-08-05 02:55:46 +0800

    Rendered database entries

--- civiclink/views.py
+++ civiclink/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import CivicLink
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def CivicLink(request):
+    civic_links = CivicLink.objects.all()
+    return render(request, "civiclink.html", {"civic_links": civic_links}) 
